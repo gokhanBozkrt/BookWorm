cbb56c403a0ecc67031830864b95c42ae804bd28
Gokhnan Bozkurt <gkhnbzkrt1000@gmail.com>
2022-05-12 21:43:11 +0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Gokhan Bozkurt on 12.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
